b62152ea863c4f85589c7961ca3ddd07d84dc0e3
Brandon Bloch <brandon@bloch.ca>
2021-12-14 12:49:59 -0500

    Use a tag to avoid caching issues

--- licensebot-check.sh
+++ licensebot-check.sh
@@ -1,2 +1,2 @@
-go install -v github.com/MosaicManufacturing/licensebot-client-go@latest || exit
+go install -v github.com/MosaicManufacturing/licensebot-client-go@0.1.0 || exit
 licensebot-client-go check
--- licensebot-update.sh
+++ licensebot-update.sh
@@ -1,2 +1,2 @@
-go install github.com/MosaicManufacturing/licensebot-client-go@latest || exit
+go install github.com/MosaicManufacturing/licensebot-client-go@0.1.0 || exit
 licensebot-client-go update
