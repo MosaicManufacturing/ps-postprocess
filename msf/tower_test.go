ac1d43a1021439104f1a89219112d79b73a930aa
Brandon Bloch <brandon@bloch.ca>
2021-12-20 16:34:11 -0500

    Add more test cases

--- msf/tower_test.go
+++ msf/tower_test.go
@@ -5,7 +5,71 @@ import (
     "testing"
 )
 
-func Test_Towers1(t *testing.T) {
+// transition length >= 80 mm
+// no infill transitions
+// no variable-length transitions
+// no tower brims needed
+func Test_TowersBasic(t *testing.T) {
+    palette := getTestPalette(80)
+    printContent := `
+;START_OF_PRINT
+T0
+G1 Z0
+;LAYER_CHANGE
+;Z:0.2
+;HEIGHT:0.2
+; printing object model id:0 copy 0
+G1 X0 Y0 Z0.2 F1800 ; move to a real XYZ position
+G1 E200 F2400 ; avoid testing logic specific to first piece length
+G92 E0
+T1
+G1 E80 F2400
+G92 E0
+T2
+G1 E80 F2400
+G92 E0
+T1
+G1 E100 F2400
+; stop printing object model id:0 copy 0
+`
+    expectedTransitions := []Transition{
+        {
+            Layer:            0,
+            From:             0,
+            To:               1,
+            TotalExtrusion:   200,
+            TransitionLength: 80,
+            PurgeLength:      80,
+            UsableInfill:     0,
+        },
+        {
+            Layer:            0,
+            From:             1,
+            To:               2,
+            TotalExtrusion:   280,
+            TransitionLength: 80,
+            PurgeLength:      80,
+            UsableInfill:     0,
+        },
+        {
+            Layer:            0,
+            From:             2,
+            To:               1,
+            TotalExtrusion:   360,
+            TransitionLength: 80,
+            PurgeLength:      80,
+            UsableInfill:     0,
+        },
+    }
+    preflightResults := testTowerPreflight(t, &palette, printContent, expectedTransitions)
+    testTowerOutput(t, &palette, printContent, &preflightResults, sequences.NewLocals())
+}
+
+// transition length < 80 mm, but enough extrusion on print to avoid piece length issues
+// no infill transitions
+// no variable-length transitions
+// no tower brims needed
+func Test_TowersShortTransitions(t *testing.T) {
     palette := getTestPalette(30)
     printContent := `
 ;START_OF_PRINT
@@ -61,7 +125,8 @@ G1 E100 F2400
     testTowerOutput(t, &palette, printContent, &preflightResults, sequences.NewLocals())
 }
 
-func Test_Towers2(t *testing.T) {
+// todo: explanatory comment
+func Test_TowersExtraPurge(t *testing.T) {
     palette := getTestPalette(30)
     printContent := `
 ;START_OF_PRINT
@@ -117,7 +182,8 @@ G1 E50 F2400
     testTowerOutput(t, &palette, printContent, &preflightResults, sequences.NewLocals())
 }
 
-func Test_Towers3(t *testing.T) {
+// todo: explanatory comment
+func Test_TowersMultipleExtraPurges(t *testing.T) {
     palette := getTestPalette(30)
     printContent := `
 ;START_OF_PRINT
