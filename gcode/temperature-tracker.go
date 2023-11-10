d5dc7d7ecd043765d7b43abeaa2d0ab679bebfdc
Brandon Bloch <brandon@bloch.ca>
2023-11-10 16:25:53 -0500

    Fallback to S parameter in heat and wait commands

--- gcode/temperature-tracker.go
+++ gcode/temperature-tracker.go
@@ -10,17 +10,35 @@ func (tt *TemperatureTracker) TrackInstruction(instruction Command) {
 	if len(instruction.Command) == 0 {
 		return
 	}
-	if instruction.Command == "M104" || instruction.Command == "M109" {
-		if s, ok := instruction.Params["s"]; ok {
-			tt.Extruder = s
+	if instruction.Command == "M104" {
+		if temp, ok := instruction.Params["s"]; ok {
+			tt.Extruder = temp
 		}
-	} else if instruction.Command == "M140" || instruction.Command == "M190" {
-		if s, ok := instruction.Params["s"]; ok {
-			tt.Bed = s
+	} else if instruction.Command == "M109" {
+		if temp, ok := instruction.Params["s"]; ok {
+			tt.Extruder = temp
+		} else if temp, ok = instruction.Params["r"]; ok {
+			tt.Extruder = temp
 		}
-	} else if instruction.Command == "M141" || instruction.Command == "M191" {
-		if s, ok := instruction.Params["s"]; ok {
-			tt.Chamber = s
+	} else if instruction.Command == "M140" {
+		if temp, ok := instruction.Params["s"]; ok {
+			tt.Bed = temp
+		}
+	} else if instruction.Command == "M190" {
+		if temp, ok := instruction.Params["s"]; ok {
+			tt.Bed = temp
+		} else if temp, ok = instruction.Params["r"]; ok {
+			tt.Bed = temp
+		}
+	} else if instruction.Command == "M141" {
+		if temp, ok := instruction.Params["s"]; ok {
+			tt.Chamber = temp
+		}
+	} else if instruction.Command == "M191" {
+		if temp, ok := instruction.Params["s"]; ok {
+			tt.Chamber = temp
+		} else if temp, ok = instruction.Params["r"]; ok {
+			tt.Chamber = temp
 		}
 	}
 }
